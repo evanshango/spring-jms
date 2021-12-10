30ceef877ca7a631ce320679951359b2be5a848c
evanshango <evansonshango@gmail.com>
2021-12-10 13:11:19 +0300

    create a task config class

--- src/main/java/com/codewithevans/springjms/SpringJmsApplication.java
+++ src/main/java/com/codewithevans/springjms/SpringJmsApplication.java
@@ -1,13 +1,22 @@
 package com.codewithevans.springjms;
 
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJmsApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+                .setPersistenceEnabled(false)
+                .setJournalDirectory("target/data/journal")
+                .setSecurityEnabled(false)
+                .addAcceptorConfiguration("invm", "vm://0")
+        ).start();
+
         SpringApplication.run(SpringJmsApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/codewithevans/springjms/model/HelloWorldMessage.java
@@ -0,0 +1,22 @@
+package com.codewithevans.springjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    private static final long serialVersionUID = -2739450948806941141L;
+    
+    private UUID id;
+    private String message;
+}
